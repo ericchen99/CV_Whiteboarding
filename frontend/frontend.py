f59ac055cae016ae8ff7dcbb62f2f862abdd3d72
Deepak Subramanian <deepaksm@umich.edu>
2021-04-04 14:06:14 -0500

    Fix

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -4,7 +4,7 @@ import socketserver
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
-            self.path = 'frontend.html'
+            self.path = 'frontend/frontend.html'
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 # Create an object of the above class
