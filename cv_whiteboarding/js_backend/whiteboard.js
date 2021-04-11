f508441f45532c35d23714a90095bdca17971d5e
ericchen99 <ericchaichen@gmail.com>
2021-04-11 17:31:13 -0400

    begin adding mask code

--- cv_whiteboarding/js_backend/whiteboard.js
+++ cv_whiteboarding/js_backend/whiteboard.js
@@ -1,3 +1,4 @@
+// https://answers.opencv.org/question/222770/hsv-white-color-range-in-js/
 let video = document.getElementById("videoInput"); // video is the id of video tag
         navigator.mediaDevices.getUserMedia({ video: true, audio: false })
         .then(function(stream) {
@@ -27,17 +28,30 @@ function onOpenCvReady() {
     canvasFrame.height = height
     let context = canvasFrame.getContext("2d");
     let src = new cv.Mat(height, width, cv.CV_8UC4);
-    let dst = new cv.Mat(height, width, cv.CV_8UC1);
+    let dst = new cv.Mat(height, width, src.type());
     const FPS = 30;
     // canvasFrame.style.display = 'none'
+    
+    let kernel = cv.Mat.ones(5, 5, src.type());
 
+    x = 0
+    y = 0
+    noiseth = 800
     function processVideo() {
         let begin = Date.now();
         context.drawImage(video, 0, 0, width, height);
         src.data.set(context.getImageData(0, 0, width, height).data);
-        cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
+        // console.log(src.data)
+        cv.cvtColor(src, dst, cv.COLOR_BGR2HSV);
+    
+        cv.inRange(dst, lower_range, upper_range, dst)
+        console.log(dst.data)
+        // mask = cv.erode(mask, kernel, iterations=1)
+        // mask = cv.dilate(mask, kernel, iterations=2)
+
         cv.imshow("canvasOutput", dst); // canvasOutput is the id of another <canvas>;
-        // schedule next one.
+        
+        // schedule next frame.
         let delay = 1000/FPS - (Date.now() - begin);
         setTimeout(processVideo, delay);
     }
@@ -53,3 +67,5 @@ function start() {
         
     }
 }
+
+
