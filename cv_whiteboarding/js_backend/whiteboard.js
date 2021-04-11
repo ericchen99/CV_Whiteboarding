a7d50776e118047af9a2c12aeca60a3e4498f86a
ericchen99 <ericchaichen@gmail.com>
2021-04-11 16:46:12 -0400

    somewhat working opencv js

--- cv_whiteboarding/js_backend/whiteboard.js
+++ cv_whiteboarding/js_backend/whiteboard.js
@@ -7,29 +7,49 @@ let video = document.getElementById("videoInput"); // video is the id of video t
         .catch(function(err) {
             console.log("An error occurred! " + err);
         });
+let height = video.videoHeight
+let width = video.videoWidth;
+
+video.addEventListener( "loadedmetadata", function (e) {
+    width = this.videoWidth,
+    height = this.videoHeight;
+    console.log(width, height)
+}, false );
         
-function openCvReady() {
-    cv['onRuntimeInitialized']=()=>{
+function onOpenCvReady() {
+
+    let height = video.videoHeight;
+    let width = video.videoWidth;
+    video.style.display = 'none';
+
+    let canvasFrame = document.getElementById("canvasFrame"); // canvasFrame is the id of <canvas>
+    canvasFrame.width = width
+    canvasFrame.height = height
+    let context = canvasFrame.getContext("2d");
+    let src = new cv.Mat(height, width, cv.CV_8UC4);
+    let dst = new cv.Mat(height, width, cv.CV_8UC1);
+    const FPS = 30;
+    // canvasFrame.style.display = 'none'
+
+    function processVideo() {
+        let begin = Date.now();
+        context.drawImage(video, 0, 0, width, height);
+        src.data.set(context.getImageData(0, 0, width, height).data);
+        cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
+        cv.imshow("canvasOutput", dst); // canvasOutput is the id of another <canvas>;
+        // schedule next one.
+        let delay = 1000/FPS - (Date.now() - begin);
+        setTimeout(processVideo, delay);
+    }
+    // schedule first one.
+    setTimeout(processVideo, 0);
+}
+
+function start() {
+    try {
+        onOpenCvReady();
+    }
+    catch(e) {
         
-        let height = video.height;
-        let width = video.width;
-        
-        let canvasFrame = document.getElementById("canvasFrame"); // canvasFrame is the id of <canvas>
-        let context = canvasFrame.getContext("2d");
-        let src = new cv.Mat(height, width, cv.CV_8UC4);
-        let dst = new cv.Mat(height, width, cv.CV_8UC1);
-        const FPS = 30;
-        function processVideo() {
-            let begin = Date.now();
-            context.drawImage(video, 0, 0, width, height);
-            src.data.set(context.getImageData(0, 0, width, height).data);
-            cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
-            cv.imshow("canvasOutput", dst); // canvasOutput is the id of another <canvas>;
-            // schedule next one.
-            let delay = 1000/FPS - (Date.now() - begin);
-            setTimeout(processVideo, delay);
-        }
-        // schedule first one.
-        setTimeout(processVideo, 0);
-    };
-  }
+    }
+}
